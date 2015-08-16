9fb306b926d9cf5e5910323423583b2afd280030
Pavel Pomerantsev <pomerantsevp@gmail.com>
2015-08-16 15:12:22 +0300

    Receive a JSON string with popular movies from theMovieDB

---
+++ README.md
@@ -0,0 +1,7 @@
+Provide your own theMovieDB API key. Create a file /app/src/main/res/values/api_keys.xml with the following contents:
+```xml
+<?xml version="1.0" encoding="utf-8"?>
+<resources>
+    <string name="movie_db_key">[YOUR_API_KEY]</string>
+</resources>
+```
--- app/src/main/java/ru/pomerantsevp/udacity/popularmovies/MainActivityFragment.java
+++ app/src/main/java/ru/pomerantsevp/udacity/popularmovies/MainActivityFragment.java
@@ -1,13 +1,22 @@
 package ru.pomerantsevp.udacity.popularmovies;
 
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 /**
@@ -50,5 +59,104 @@ public class MainActivityFragment extends Fragment {
         listView.setAdapter(mListAdapter);
 
         return rootView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        updateMovies();
+    }
+
+    private void updateMovies() {
+        new FetchMoviesTask().execute();
+    }
+
+    private class FetchMoviesTask extends AsyncTask<Void, Void, String[]> {
+        private final String TAG = FetchMoviesTask.class.getSimpleName();
+
+        @Override
+        protected String[] doInBackground(Void... params) {
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String movieListJsonStr = null;
+
+            try {
+                // Construct the URL for theMovieDB query
+                Uri builtUri = Uri.parse("http://api.themoviedb.org/3/discover/movie").buildUpon()
+                        .appendQueryParameter("sort_by", "popularity.desc")
+                        .appendQueryParameter("api_key", getString(R.string.movie_db_key))
+                        .build();
+                URL url = new URL(builtUri.toString());
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                movieListJsonStr = buffer.toString();
+            } catch (IOException e) {
+                Log.e(TAG, "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attempting
+                // to parse it.
+                return null;
+            } finally{
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(TAG, "Error closing stream", e);
+                    }
+                }
+            }
+
+            Log.d(TAG, movieListJsonStr);
+
+//            try {
+//                return getWeatherDataFromJson(forecastJsonStr, numDays);
+//            } catch (JSONException e) {
+//                Log.e(LOG_TAG, e.getMessage(), e);
+//                e.printStackTrace();
+//            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String[] strings) {
+            if (strings != null) {
+                mListAdapter.clear();
+                for (String string : strings) {
+                    mListAdapter.add(string);
+                }
+            }
+        }
     }
 }
