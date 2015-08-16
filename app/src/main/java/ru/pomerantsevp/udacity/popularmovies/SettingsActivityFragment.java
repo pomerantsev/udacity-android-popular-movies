7713a39038482915f140643c85ff26894f1d6d60
Pavel Pomerantsev <pomerantsevp@gmail.com>
2015-08-16 20:21:57 +0300

    Display preference summary

--- app/src/main/java/ru/pomerantsevp/udacity/popularmovies/SettingsActivityFragment.java
+++ app/src/main/java/ru/pomerantsevp/udacity/popularmovies/SettingsActivityFragment.java
@@ -1,12 +1,16 @@
 package ru.pomerantsevp.udacity.popularmovies;
 
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class SettingsActivityFragment extends PreferenceFragment {
+public class SettingsActivityFragment extends PreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
 
     public SettingsActivityFragment() {
     }
@@ -16,5 +20,32 @@ public class SettingsActivityFragment extends PreferenceFragment {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.preferences);
+
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_order_key)));
+    }
+
+    private void bindPreferenceSummaryToValue(Preference preference) {
+        preference.setOnPreferenceChangeListener(this);
+
+        onPreferenceChange(preference,
+                PreferenceManager
+                        .getDefaultSharedPreferences(preference.getContext())
+                        .getString(preference.getKey(), ""));
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        String stringValue = newValue.toString();
+
+        if (preference instanceof ListPreference) {
+            // For list preferences, look up the correct display value in
+            // the preference's 'entries' list (since they have separate labels/values).
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(stringValue);
+            if (prefIndex >= 0) {
+                preference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+        }
+        return true;
     }
 }
