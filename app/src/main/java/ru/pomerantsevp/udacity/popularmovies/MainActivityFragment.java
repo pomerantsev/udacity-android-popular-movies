6bd8ed17b7e63a8a7a09694608011553a39e6ac3
Pavel Pomerantsev <pomerantsevp@gmail.com>
2015-08-16 13:43:17 +0300

    Display a dummy list of movies

--- app/src/main/java/ru/pomerantsevp/udacity/popularmovies/MainActivityFragment.java
+++ app/src/main/java/ru/pomerantsevp/udacity/popularmovies/MainActivityFragment.java
@@ -1,15 +1,21 @@
 package ru.pomerantsevp.udacity.popularmovies;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private ArrayAdapter<String> mListAdapter;
 
     public MainActivityFragment() {
     }
@@ -17,6 +23,32 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        String[] movieTitles = {
+                "Whiplash",
+                "Birdman",
+                "Interstellar",
+                "Avengers: Age of Ultron",
+                "Mad Max: Fury Road",
+                "Ant-Man",
+                "Guardians of the Galaxy",
+                "Kingsman: Secret Service"
+        };
+        ArrayList<String> movieTitlesList = new ArrayList<>();
+        for (String title : movieTitles) {
+            movieTitlesList.add(title);
+        }
+
+        ListView listView = (ListView) rootView.findViewById(R.id.movies_list);
+
+        mListAdapter = new ArrayAdapter<>(
+                getActivity(),
+                R.layout.list_item_movie,
+                movieTitlesList
+        );
+        listView.setAdapter(mListAdapter);
+
+        return rootView;
     }
 }
