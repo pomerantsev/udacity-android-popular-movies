1cd5311fd7d6a07346c91b56405db3828a1fa1c4
Pavel Pomerantsev <pomerantsevp@gmail.com>
2015-08-19 00:55:04 +0300

    Output date and rating in a better way

--- app/src/main/java/ru/pomerantsevp/udacity/popularmovies/DetailActivityFragment.java
+++ app/src/main/java/ru/pomerantsevp/udacity/popularmovies/DetailActivityFragment.java
@@ -15,6 +15,10 @@ import com.squareup.picasso.Picasso;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * A placeholder fragment containing a simple view.
  */
@@ -33,16 +37,28 @@ public class DetailActivityFragment extends Fragment {
         if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
             try {
                 JSONObject movie = new JSONObject(intent.getStringExtra(Intent.EXTRA_TEXT));
+
                 String imageUrl = SharedConstants.IMAGE_PATH_PREFIX +
                         movie.getString(SharedConstants.POSTER_PATH);
                 ImageView poster = (ImageView) rootView.findViewById(R.id.poster);
                 Picasso.with(activity).load(imageUrl).into(poster);
+
                 TextView title = (TextView) rootView.findViewById(R.id.title);
                 title.setText(movie.getString("original_title"));
+
                 TextView releaseDate = (TextView) rootView.findViewById(R.id.release_date);
-                releaseDate.setText(movie.getString("release_date"));
+                SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd");
+                SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy");
+                try {
+                    Date date = inputFormat.parse(movie.getString("release_date"));
+                    releaseDate.setText(outputFormat.format(date));
+                } catch (ParseException e) {
+                    // ignore
+                }
+
                 TextView rating = (TextView) rootView.findViewById(R.id.rating);
-                rating.setText(movie.getString("vote_average"));
+                rating.setText(movie.getString("vote_average") + "/10");
+
                 TextView plotSynopsis = (TextView) rootView.findViewById(R.id.plot_synopsis);
                 plotSynopsis.setText(movie.getString("overview"));
             } catch (JSONException e) {
